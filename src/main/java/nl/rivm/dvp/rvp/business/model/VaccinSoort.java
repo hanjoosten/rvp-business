1b481f54f807d8c03f6725a6284aff983b07c358
hanjoosten <han.joosten@ordina.nl>
2023-04-07 10:57:36 +0200

    Vaccinsoort enumeratie aangevuld

--- src/main/java/nl/rivm/dvp/rvp/business/model/VaccinSoort.java
+++ src/main/java/nl/rivm/dvp/rvp/business/model/VaccinSoort.java
@@ -6,23 +6,29 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum VaccinSoort {
-   IPV, // Geïnactiveerd poliovaccin (IPV): Dit vaccin biedt bescherming tegen polio.
-   BMR, // BMR-vaccin (bof, mazelen, rodehond): Dit vaccin biedt bescherming tegen bof, mazelen en rodehond.
-   DTP, // DTP-vaccin (difterie, tetanus, pertussis): Dit vaccin biedt bescherming tegen difterie, tetanus en pertussis (kinkhoest).
-   DKTP, 
-   DKT,
-   HIB, // Hib-vaccin (Haemophilus influenzae type b): Dit vaccin biedt bescherming tegen infecties veroorzaakt door de bacterie Haemophilus influenzae type b.
-   PCV, // Pneumokokkenvaccin (PCV): Dit vaccin biedt bescherming tegen infecties veroorzaakt door de bacterie Streptococcus pneumoniae.
-   MENACWY, //MenACWY-vaccin (meningokokken ACWY): Dit vaccin biedt bescherming tegen infecties veroorzaakt door de bacterie Neisseria meningitidis serogroepen A, C, W en Y.
-   HPV // HPV-vaccin (humaan papillomavirus): Dit vaccin biedt bescherming tegen infecties veroorzaakt door het humaan papillomavirus, dat kan leiden tot baarmoederhalskanker en andere kankers.
-}
+   AK, // Het losse aK-vaccin (acellulair Kinkhoest)
+   BMR, // BMR-vaccin (bof, mazelen, rodehond): Dit vaccin biedt bescherming tegen bof,
+        // mazelen en rodehond.
+   DKT, // Het DKT-vaccin (een cocktail van D, K en T) wat wordt ingezet om zwangeren te
+        // beschermen tegen kinkhoest
+   DKTP_BOOSTER, // - Het DKTP booster vaccin (een cocktail van D, K, T en P maar met een lagere
+                 // dosis waardoor het uitsluitend ingezet kan worden als booster vaccin)
+   DKTP_HEPB, // Het DKTP-HepB vaccin
+   DKTP_HIB, // Het DKTP-Hib vaccin
 
-// Het Rijksvaccinatieprogramma (RVP) in Nederland maakt momenteel gebruik van de volgende vaccinsoorten:
-// 1. Geïnactiveerd poliovaccin (IPV): Dit vaccin biedt bescherming tegen polio.
-// 2. BMR-vaccin (bof, mazelen, rodehond): Dit vaccin biedt bescherming tegen bof, mazelen en rodehond.
-// 3. DTP-vaccin (difterie, tetanus, pertussis): Dit vaccin biedt bescherming tegen difterie, tetanus en pertussis (kinkhoest).
-// 4. Hib-vaccin (Haemophilus influenzae type b): Dit vaccin biedt bescherming tegen infecties veroorzaakt door de bacterie Haemophilus influenzae type b.
-// 5. Pneumokokkenvaccin (PCV): Dit vaccin biedt bescherming tegen infecties veroorzaakt door de bacterie Streptococcus pneumoniae.
-// 6. MenACWY-vaccin (meningokokken ACWY): Dit vaccin biedt bescherming tegen infecties veroorzaakt door de bacterie Neisseria meningitidis serogroepen A, C, W en Y.
-// 7. HPV-vaccin (humaan papillomavirus): Dit vaccin biedt bescherming tegen infecties veroorzaakt door het humaan papillomavirus, dat kan leiden tot baarmoederhalskanker en andere kankers.
-// Het Rijksvaccinatieprogramma is aan veranderingen onderhevig en kan in de toekomst vaccins van andere types bevatten, afhankelijk van de aanbevelingen van de Gezondheidsraad.
+   DKTP_HIB_HEPB, // een cocktail van D – difterie, K – kinkhoest, T – tetanus, P – polio, Hib –
+   // Haemophilus influenzae type B en HepB – Hepatitis B
+   DTP, // Het DTP-vaccin (een cocktail van D, T en P)
+   HEPB, // Het losse EepB vaccin
+   HIB, // Hib-vaccin (Haemophilus influenzae type b): Dit vaccin biedt bescherming
+        // tegen infecties veroorzaakt door de bacterie Haemophilus influenzae type b.
+   HPV, // HPV-vaccin (humaan papillomavirus): Dit vaccin biedt bescherming tegen
+        // infecties veroorzaakt door het humaan papillomavirus, dat kan leiden tot
+        // baarmoederhalskanker en andere kankers.
+   MENACWY, // MenACWY-vaccin (meningokokken ACWY): Dit vaccin biedt bescherming tegen
+   // infecties veroorzaakt door de bacterie Neisseria meningitidis serogroepen A,
+   // C, W en Y.
+   MENC, // Het MenC-vaccin (Meningokokken type C)
+   PNEU, // Pneumokokkenvaccin: Dit vaccin biedt bescherming tegen infecties veroorzaakt
+         // door de bacterie Streptococcus pneumoniae.
+}
