9a467ae16ed03413909500256fb7ed32578c6d02
hanjoosten <han.joosten@ordina.nl>
2023-05-06 10:08:03 +0200

    vaccinatieToestand opgenomen in vaccinatieResultaat

--- src/main/java/nl/rivm/dvp/rvp/business/model/VaccinatieToestand.java
+++ src/main/java/nl/rivm/dvp/rvp/business/model/VaccinatieToestand.java
@@ -1,11 +1,17 @@
 package nl.rivm.dvp.rvp.business.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public enum VaccinatieToestand {
   // using the constructor defined below
 
+  @JsonProperty("Basisimmuun")
   BASISIMMUUN("Basisimmuun"),
+  @JsonProperty("Gerevaccineerd")
   GEREVACCINEERD("Gerevaccineerd"),
+  @JsonProperty("Primaire serie")
   PRIMAIRE_SERIE("Primaire serie"),
+  @JsonProperty("Volledig afgesloten")
   VOLLEDIGAFGESLOTEN("Volledig afgesloten");
 
   // Member to hold the name
