c3d2f1b6c0a35e37ea3b737d34ac7f044b8eb106
hanjoosten <han.joosten@ordina.nl>
2023-05-09 10:46:02 +0200

    Enumeraties

--- src/main/java/nl/rivm/dvp/rvp/business/model/Component.java
+++ src/main/java/nl/rivm/dvp/rvp/business/model/Component.java
@@ -1,32 +1,60 @@
 package nl.rivm.dvp.rvp.business.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public enum Component {
 
+    @JsonProperty("aK")
     AK("aK"),
+    @JsonProperty("B")
     B("B"),
+    @JsonProperty("BCG")
     BCG("BCG"),
+    @JsonProperty("COVID")
     COVID("COVID"),
+    @JsonProperty("D")
     D("D"),
+    @JsonProperty("D booster")
     DBOOSTER("D booster"),
+    @JsonProperty("HBIg")
     HBIG("HBIg"),
+    @JsonProperty("HepA")
     HEPA("HepA"),
+    @JsonProperty("HepB")
     HEPB("HepB"),
+    @JsonProperty("Hib")
     HIB("Hib"),
+    @JsonProperty("HPV")
     HPV("HPV"),
+    @JsonProperty("IPV")
     IPV("IPV"),
+    @JsonProperty("K")
     K("K"),
+    @JsonProperty("K booster")
     KBOOSTER("K booster"),
+    @JsonProperty("M")
     M("M"),
+    @JsonProperty("MenACWY")
     MENACWY("MenACWY"),
+    @JsonProperty("MenC")
     MENC("MenC"),
+    @JsonProperty("OPV")
     OPV("OPV"),
+    @JsonProperty("P")
     P("P"),
+    @JsonProperty("P booster")
     PBOOSTER("P booster"),
+    @JsonProperty("Pneu")
     PNEU("Pneu"),
+    @JsonProperty("R")
     R("R"),
+    @JsonProperty("Rota")
     ROTA("Rota"),
+    @JsonProperty("T")
     T("T"),
+    @JsonProperty("T booster")
     TBOOSTER("T booster"),
+    @JsonProperty("Waterpokken")
     WATERPOKKEN("Waterpokken");
 
     // Member to hold the name
--- src/main/java/nl/rivm/dvp/rvp/business/model/VaccinSoort.java
+++ src/main/java/nl/rivm/dvp/rvp/business/model/VaccinSoort.java
@@ -1,45 +1,87 @@
 package nl.rivm.dvp.rvp.business.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public enum VaccinSoort {
+     @JsonProperty("aK")
      AK("aK"),
+     @JsonProperty("B")
      B("B"),
+     @JsonProperty("BCG")
      BCG("BCG"),
+     @JsonProperty("BM")
      BM("BM"),
+     @JsonProperty("BMR")
      BMR("BMR"),
+     @JsonProperty("BMRW")
      BMRW("BMRW"),
+     @JsonProperty("COVID")
      COVID("COVID"),
+     @JsonProperty("D")
      D("D"),
+     @JsonProperty("DK-Hib")
      DKHIB("DK-Hib"),
+     @JsonProperty("DKT")
      DKT("DKT"),
+     @JsonProperty("DKT+HepB")
      DKTHEPB("DKT+HepB"),
+     @JsonProperty("DKT+Hib")
      DKTHIB("DKT+Hib"),
+     @JsonProperty("DKT+Hib+HepB")
      DKTHIBHEPB("DKT+Hib+HepB"),
+     @JsonProperty("DKTP booster")
      DKTPBOOSTER("DKTP booster"),
+     @JsonProperty("DKTP-HepB")
      DKTPHEPB("DKTP-HepB"),
+     @JsonProperty("DKTP-Hib-HepB")
      DKTPHIBHEPB("DKTP-Hib-HepB"),
+     @JsonProperty("DKTP-Hib")
      DKTPHIB("DKTP-Hib"),
+     @JsonProperty("DKTP")
      DKTP("DKTP"),
+     @JsonProperty("DT")
      DT("DT"),
+     @JsonProperty("DTP")
      DTP("DTP"),
+     @JsonProperty("HBIg")
      HBIG("HBIg"),
+     @JsonProperty("Hep A")
      HEPA("Hep A"),
+     @JsonProperty("Hep B")
      HEPB("Hep B"),
+     @JsonProperty("Hib")
      HIB("Hib"),
+     @JsonProperty("Hib+HepB")
      HIBHEPB("Hib+HepB"),
+     @JsonProperty("HPV")
      HPV("HPV"),
+     @JsonProperty("IPV")
      IPV("IPV"),
+     @JsonProperty("K")
      K("K"),
+     @JsonProperty("M")
      M("M"),
+     @JsonProperty("MenACWY")
      MENACWY("MenACWY"),
+     @JsonProperty("MenC")
      MENC("MenC"),
+     @JsonProperty("MR")
      MR("MR"),
+     @JsonProperty("OPV")
      OPV("OPV"),
+     @JsonProperty("P")
      P("P"),
+     @JsonProperty("Pneu")
      PNEU("Pneu"),
+     @JsonProperty("R")
      R("R"),
+     @JsonProperty("Rota")
      ROTA("Rota"),
+     @JsonProperty("T")
      T("T"),
+     @JsonProperty("TP")
      TP("TP"),
+     @JsonProperty("Waterpokken")
      WATERPOKKEN("Waterpokken");
 
      // Member to hold the name
